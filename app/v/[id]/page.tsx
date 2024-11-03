f12794ee68b0e4f0ad297288b6d15cb129026d36
promsides <161449988+promsides@users.noreply.github.com>
2024-11-04 02:30:40 +0700

    Update page.tsx

--- app/v/[id]/page.tsx
+++ app/v/[id]/page.tsx
@@ -82,6 +82,26 @@ export default async function Video({ params }: PageProps) {
     }
 
     const file = data.result[0];
+const jsonLd = {
+        '@context': 'https://schema.org',
+        '@type': 'VideoObject',
+        name: `${file.title} - ${SITENAME}`,
+        thumbnailUrl: file.splash_img,
+        description: `${file.title} di ${SITENAME} Video Bokep Indo Jepang Jav Barat Simontok Viral Terbaru Bocil Ngentot Jilbab Smp Mama Sma`,
+        url: `https://bokepindoartis.pages.dev/v/${file.filecode}`,
+        embedUrl: `https://doodstream.com/e/${file.filecode}`,
+        uploadDate: new Date(
+            file.uploaded + ".000Z"
+        ).toISOString(),
+        interactionStatistic: {
+            '@type': `InteractionCounter`,
+                userInteractionCount: `${file.views}`,
+            interactionType: {
+                '@type': `WatchAction`,
+                target: `https://bokepindoartis.pages.dev/v/${file.filecode}`
+            }  
+        }
+        }
         const jsonLd2 = {
         '@context': 'https://schema.org',
         '@type': 'Article',
@@ -127,6 +147,10 @@ export default async function Video({ params }: PageProps) {
         {/* Add JSON-LD to your page */}
         <script
           type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+        />
+        <script
+          type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd2) }}
         />
         <script
@@ -137,7 +161,7 @@ export default async function Video({ params }: PageProps) {
         </section>
             <iframe
                 className="w-full h-[30vh] md:h-[55vh] lg:h-[70vh]"
-                src={`https://${upstream}/e/${file.filecode}`}
+                src={`https://doodstream.com/e/${file.filecode}`}
                 scrolling="no"
                 title={file.title}
                 frameBorder={0}
